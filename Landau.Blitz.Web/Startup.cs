92fff476ba0be0fa2574d203fafef2284d5f76b4
vyadzmak <vyadzmak@gmail.com>
2017-05-17 18:49:05 +0300

    Second Commit

---
+++ Landau.Blitz.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Landau.Blitz.Web.Startup))]
+namespace Landau.Blitz.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
